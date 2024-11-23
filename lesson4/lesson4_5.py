0e19c6d8a407728ffca5ff88c4be7d5f4be4a94e
shuya1123 <shuya7711@gmail.com>
2024-11-23 16:15:11 +0800

    commit

---
+++ lesson4/lesson4_5.py
@@ -0,0 +1,3 @@
+import tools
+
+s1 = tools.Student('小張',age=17,score=245).echo()
--- lesson4/tools.py
+++ lesson4/tools.py
@@ -33,3 +33,26 @@ def BMI_math(height_cm:float, weight_kg:float)->tuple[float,str]:
     bmi_kg_m2 = round(weight_kg/(height_m**2), 2)
     bmi_str = get_status(bmi_kg_m2)
     return bmi_kg_m2, bmi_str
+
+class Person:
+    def __init__(self,name:str,age:int):
+        self.name = name #attribute
+        self.age = age #attribute
+
+    #實體方法(instance method)
+    def echo(self):
+        print(f'我的姓名是:{self.name}')
+        print(f'我的age是:{self.age}')
+
+class Student(Person):
+    def __init__(self,name:str,age:int,score:int):
+        super().__init__(name=name,age=age)
+        self.__score = score
+
+    @property
+    def score(self)->int:
+        return self.__score
+
+    def echo(self):
+        super().echo()
+        print(f'我的score是:{self.score}')
