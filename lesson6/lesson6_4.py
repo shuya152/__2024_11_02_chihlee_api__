9cd23347cdc17cddbea3c0d6780f2984e3f0f1ac
shuya <shuya7711@gmail.com>
2024-12-07 15:54:58 +0800

    commit

--- lesson6/lesson6_4.py
+++ lesson6/lesson6_4.py
@@ -6,7 +6,9 @@ import tools
 
 sitenames:list[str] = tools.get_sitenames(excel_name='aqi.xlsx')
 # https://docs.streamlit.io/develop/api-reference/widgets/st.selectbox
-add_selectbox = st.sidebar.selectbox(
-    '請點選戰點名稱:',
-    sitenames
-)
+# add_selectbox = st.sidebar.selectbox('請點選戰點名稱:',sitenames)
+with st.sidebar:
+    add_selectbox = st.selectbox('請點選戰點名稱:',sitenames)
+
+    # https://docs.streamlit.io/develop/api-reference/text/st.title
+    st.title(f"{add_selectbox}")
--- lesson6/新增資料夾/lesson6_4.py
+++ lesson6/新增資料夾/lesson6_4.py
@@ -3,7 +3,17 @@ import tools
 
 sitenames:list[str] = tools.get_sitenames(excel_name='aqi.xlsx')
 # Object notation
-add_selectbox = st.sidebar.selectbox(
-    "請選擇站點名稱:",
-    sitenames 
-)
+#add_selectbox = st.sidebar.selectbox(
+#    "請選擇站點名稱:",
+#    sitenames 
+#)
+
+# "with" notation
+
+with st.sidebar:
+    add_selectbox = st.selectbox(
+       "請選擇站點名稱:",
+        sitenames  
+    )
+
+    st.title(f"{add_selectbox}")
