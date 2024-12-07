708d6dd38053225b0ed443666c040912339614a1
shuya <shuya7711@gmail.com>
2024-12-07 15:39:38 +0800

    commit

--- lesson6/新增資料夾/lesson6_4.py
+++ lesson6/新增資料夾/lesson6_4.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import tools
 
+sitenames:list[str] = tools.get_sitenames(excel_name='aqi.xlsx')
+# Object notation
 add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
+    "請選擇站點名稱:",
+    sitenames 
 )
