2e035765e03b4891f07cf28693ae8937a3d37677
shuya <shuya7711@gmail.com>
2024-12-21 10:36:58 +0800

    commit

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -12,4 +12,10 @@ def increment_counter(**kwargs):
     st.session_state.counter -= kwargs['second']
 
 st.header(f"這個網頁已經執行{st.session_state.counter}次")
-st.button("再執行一次", key="reset", help="按下+1",on_click=increment_counter,kwargs={'first':5,'second':3}) #自動建立reset_key
+st.button(
+    "再執行一次",
+    key="reset",
+    help="按下+1",
+    on_click=increment_counter,
+    kwargs={'first':5,'second':3}
+)#自動建立reset_key
