225d8e8c2fa7c81b5cd7b893ea2ffb43e7b3a0fe
shuya <shuya7711@gmail.com>
2024-12-21 10:23:33 +0800

    commit

--- lesson8/lesson8_1.py
+++ lesson8/lesson8_1.py
@@ -7,8 +7,9 @@ if "counter" not in st.session_state:
 
 # st.session_state.counter += 1
 
-def increment_counter():
-    st.session_state.counter += 1
+def increment_counter(*args):
+    st.session_state.counter += args[0]
+    st.session_state.counter -= args[1]
 
 st.header(f"這個網頁已經執行{st.session_state.counter}次")
-st.button("再執行一次", key="reset", help="按下+1",on_click=increment_counter) #自動建立reset_key
+st.button("再執行一次", key="reset", help="按下+1",on_click=increment_counter,args=(5,3)) #自動建立reset_key
